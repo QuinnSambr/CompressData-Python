cb0b033b1c60fc32de0db814e11588b0fe941558
Quinn Sambrano <Quinn_ISambrano@hotmail.com>
2019-08-28 15:27:19 -0400

    Update

--- compress.py
+++ compress.py
@@ -1,29 +1,26 @@
 import zipfile
-import os 
+import os , glob 
 import datetime as dt
 import os.path, time
 import pandas as pd #pip install pandas
 
 fileMonthPrev = dt.datetime.now() - dt.timedelta(days=30)
-fileMonthFormat =fileMonthPrev.strftime("%Y.%m.%d")
 start_date = dt.datetime.now() - dt.timedelta(days=120)
 start_date.strftime("%Y.%m.%d")
 end_date = fileMonthPrev - dt.timedelta(days=30)
 end_date.strftime("%Y.%m.%d")
-try: 
-    while end_date == fileFormat :
-        pd_range = pd.date_range(start_date, end_date)
-        pd_range.strftime("%Y.%m.%d")
-        fileCreated =dt.datetime.fromtimestamp(os.path.getctime("C:/Users/Quinn-Laptop/Documents/python/Compress/localhost_access_log."+pd_range+".log")) #File Location 
-        fileMinus = fileCreated - dt.timedelta(days=30)
-        fileFormat =fileMinus.strftime("%Y.%m.%d")
-        if (fileCreated >=  fileMinus):
-            zip_name = zipfile.ZipFile("localhost_access_log."+pd_range+".zip", "w") #Convert to Zip 
-            zip_name.write("localhost_access_log."+pd_range+".log", compress_type=zipfile.ZIP_DEFLATED) #file to be converted
-            os.remove("localhost_access_log."+pd_range+".log") # Delete Initial File
-            zip_name.close()
-            print("File Successfully Compressed")
+pd_range = (pd.date_range(start_date, end_date).strftime("%Y.%m.%d"))
+pd = (str(pd_range))
 
+temp=0
+try:
+    for name in glob.glob("*.log"):
+        temp = temp+1
+        for i in range (len(pd_range)):
+            if str(pd_range[i]) in name: 
+                zip_name = zipfile.ZipFile("C:\\Users\Quinn-Laptop\Documents\python\Compress\localhost_access_log."+str(pd_range[i])+".zip", "w") #Convert to Zip 
+                zip_name.write("localhost_access_log."+str(pd_range[i])+".log", compress_type=zipfile.ZIP_DEFLATED) #file to be converted
+                os.remove("localhost_access_log."+str(pd_range[i])+".log") # Delete Initial File
+                zip_name.close() 
 except: 
-    print("File is Not One(1) Month Old \n or")
-    print("File Location Cannot Be Found")
+    print("It Died :(")  
